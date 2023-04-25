917fd904092b97a6be636428b96c7775d7578ee4
alvin-reyes <me@areyes.onl>
2023-04-25 12:13:04 -0400

    enumerated the statistics

--- README.md
+++ README.md
@@ -4,5 +4,53 @@ Rest API for Delta Global Metrics
 make dmr
 ```
 
+## Global Stats available
+- total deals attempted
+- total e2e deals attempted
+- total import deals attempted
+- total deals succeeded
+- total e2e deals succeeded
+- total import deals succeeded
+- total deals failed
+- total e2e deals failed
+- total import deals failed
+- total deals active
+- total e2e deals active
+- total import deals active
+- total number of sps
+- total number of delta nodes
+
+## Storage stats available
+- total storage consumed by deals attempted
+- total storage consumed by e2e deals attempted
+- total storage consumed by import deals attempted
+- total storage consumed by deals succeeded
+- total storage consumed by e2e deals succeeded
+- total storage consumed by import deals succeeded
+- total storage consumed by deals failed
+- total storage consumed by e2e deals failed
+- total storage consumed by import deals failed
+- total storage consumed by deals active
+- total storage consumed by e2e deals active
+- total storage consumed by import deals active
+- total storage consumed by all deals
+- total storage consumed by all e2e deals
+
+## SP
+- list of SP
+- list of SP location
+- list of SP deals attempted
+- list of SP deals succeeded
+- list of SP deals failed
+- list of SP deals active
+- list of SP deals attempted by e2e
+- list of SP deals succeeded by e2e
+- list of SP deals failed by e2e
+- list of SP deals active by e2e
+- list of SP deals attempted by import
+- list of SP deals succeeded by import
+- list of SP deals failed by import
+
+
 # Author
 Protocol Labs Outercore Engineering.
--- dao/statistics.go
+++ dao/statistics.go
@@ -1,27 +1,85 @@
 package dao
 
+import (
+	"fmt"
+	"github.com/application-research/delta-metrics-rest/model"
+)
+
 // function to get all totals info
 func GetOpenTotalInfoStats() (interface{}, error) {
 
 	var totalContentConsumed int64
-	DB.Raw("select count(*) from content_logs").Count(&totalContentConsumed)
+	//SELECT SUM(cnt) as total_rows
+	//FROM (
+	//         SELECT COUNT(*) as cnt
+	//         FROM content_logs
+	//         GROUP BY system_content_id, delta_node_uuid
+	//     ) subquery;
+	DB.Raw("SELECT SUM(cnt) as total_rows FROM (SELECT COUNT(*) as cnt FROM content_logs GROUP BY system_content_id, delta_node_uuid) subquery").Find(&totalContentConsumed)
+	//DB.Raw("select count(*) from content_logs").Count(&totalContentConsumed)
 
 	var totalTransferStarted int64
-	DB.Raw("select count(*) from content_logs where status = 'transfer-started'").Count(&totalTransferStarted)
+	//SELECT SUM(cnt) as total_rows
+	//FROM (
+	//         SELECT COUNT(*) as cnt
+	//         FROM content_logs
+	//         WHERE status = 'transfer-started'
+	//         GROUP BY system_content_id, delta_node_uuid
+	//     ) subquery;
+	err := DB.Model(model.ContentLogs{}).Where("status = ?", "transfer-started").Count(&totalTransferStarted).Error
+	if err != nil {
+		fmt.Println("Error in GetOpenTotalInfoStats", err)
+		return nil, err
+	}
 
 	var totalTransferFinished int64
-	DB.Raw("select count(*) from content_logs where status = 'transfer-finished'").Count(&totalTransferFinished)
-
+	//SELECT SUM(cnt) as total_rows
+	//FROM (
+	//         SELECT COUNT(*) as cnt
+	//         FROM content_logs
+	//         WHERE status = 'transfer-started'
+	//         GROUP BY system_content_id, delta_node_uuid
+	//     ) subquery;
+	err = DB.Model(model.ContentLogs{}).Where("status = ?", "transfer-finished").Count(&totalTransferFinished).Error
+	if err != nil {
+		fmt.Println("Error in GetOpenTotalInfoStats", err)
+		return nil, err
+	}
 	var totalProposalMade int64
-	DB.Raw("select count(*) from content_deal_proposal_logs").Count(&totalProposalMade)
+	//SELECT SUM(cnt) as total_rows
+	//FROM (
+	//	SELECT COUNT(*) as cnt
+	//FROM content_deal_proposal_logs
+	//GROUP BY system_content_deal_proposal_id, delta_node_uuid
+	//) subquery;
+	DB.Raw("select count(*) from content_deal_proposal_logs").Find(&totalProposalMade)
 
 	var totalCommitmentPiece int64
-	DB.Raw("select count(*) from piece_commitment_logs").Count(&totalCommitmentPiece)
+	//SELECT SUM(cnt) as total_rows
+	//FROM (
+	//	SELECT COUNT(*) as cnt
+	//FROM piece_commitment_logs
+	//GROUP BY system_content_piece_commitment_id, delta_node_uuid
+	//) subquery;
+	DB.Raw("select count(*) from piece_commitment_logs").Find(&totalCommitmentPiece)
 
 	var totalPieceCommitted int64
-	DB.Raw("select count(*) from piece_commitment_logs where status = 'committed'").Count(&totalPieceCommitted)
+	//SELECT SUM(cnt) as total_rows
+	//FROM (
+	//	SELECT COUNT(*) as cnt
+	//FROM piece_commitment_logs
+	//WHERE status = 'committed'
+	//GROUP BY system_content_piece_commitment_id, delta_node_uuid
+	//) subquery;
+	DB.Raw("select count(*) from piece_commitment_logs where status = 'committed'").Find(&totalPieceCommitted)
 
 	var totalMiners int64
+	//SELECT count(cnt) as total_rows
+	//FROM (
+	//	SELECT miner as cnt
+	//FROM content_miner_logs
+	//GROUP BY miner
+	//) subquery;
 	rows, err := DB.Raw("select distinct(miner) from content_miner_logs").Rows()
 	if err != nil {
 		return nil, err
@@ -37,7 +95,7 @@ func GetOpenTotalInfoStats() (interface{}, error) {
 	DB.Raw("select sum(size) from content_logs").Count(&totalStorageAllocated)
 
 	var totalProposalSent int64
-	DB.Raw("select count(*) from content_logs where status = 'deal-proposal-sent'").Count(&totalProposalSent)
+	DB.Raw("select count(*) from content_logs c where status = 'deal-proposal-sent' and c.system_content_id in (select c.system_content_id from content_logs where status = 'deal-proposal-sent')").Count(&totalProposalSent)
 
 	var totalSealedDealInBytes int64
 	DB.Raw("select sum(size) from content_logs where status in ('transfer-started','transfer-finished','deal-proposal-sent')").Scan(&totalSealedDealInBytes)
