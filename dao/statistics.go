e24ff58793ae7eef190507677b7749d4dcacc847
alvin-reyes <me@areyes.onl>
2023-06-08 08:08:21 -0400

    add padded import and e2e deal sizes

--- dao/statistics.go
+++ dao/statistics.go
@@ -173,7 +173,8 @@ func GetOpenTotalInfoStats() (interface{}, error) {
 			statsTotal.TotalPieceCommitmentsComputeSucceededSize = totalPieceCommitmentsComputeSucceededSize
 
 			var totalE2EDealsSucceededSize int
-			row = tx.Raw("select sum(size) as total_size_sum from (select c.size as size,system_content_id from content_logs c where c.connection_mode = 'e2e' and status in ('transfer-started','transfer-finished') and (c.delta_node_uuid is not null or c.delta_node_uuid is null or c.delta_node_uuid = '')  group by c.size,system_content_id) subquery").Row()
+			row = tx.Raw("select sum(size) as total_size_sum from (select p.padded_piece_size as size,system_content_id from content_logs c, piece_commitment_logs p where c.piece_commitment_id = p.system_content_piece_commitment_id and c.status in ('transfer-started','transfer-finished') and c.connection_mode = 'e2e' and (c.delta_node_uuid is not null or c.delta_node_uuid is null or c.delta_node_uuid = '')  group by system_content_id, p.padded_piece_size) subquery").Row()
+			//
 			err = row.Scan(&totalE2EDealsSucceededSize)
 			if err != nil {
 				fmt.Println("Error in getting total e2e deals succeeded size", err)
@@ -193,7 +194,7 @@ func GetOpenTotalInfoStats() (interface{}, error) {
 			statsTotal.TotalImportSucceeded = totalImportDealsSucceeded
 
 			var totalImportDealsSucceededSize int
-			row = tx.Raw("select sum(size) as total_size_sum from (select c.size as size,system_content_id from content_logs c where c.connection_mode = 'import' and status in ('deal-proposal-sent') and (c.delta_node_uuid is not null or c.delta_node_uuid is null or c.delta_node_uuid = '')  group by c.size,system_content_id) subquery").Row()
+			row = tx.Raw("select sum(size) as total_size_sum from (select p.padded_piece_size as size,system_content_id from content_logs c, piece_commitment_logs p where c.piece_commitment_id = p.system_content_piece_commitment_id and c.status in ('deal-proposal-sent') and c.connection_mode = 'import' and (c.delta_node_uuid is not null or c.delta_node_uuid is null or c.delta_node_uuid = '')  group by system_content_id, p.padded_piece_size) subquery").Row()
 			err = row.Scan(&totalImportDealsSucceededSize)
 			if err != nil {
 				fmt.Println("Error in getting total import deals succeeded size", err)
